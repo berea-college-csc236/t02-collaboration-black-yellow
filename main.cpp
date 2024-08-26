6ff963586900381cf6d9c0819ab6f2281a8eb49d
Eun Sung Wang <156254694+esw0624@users.noreply.github.com>
2024-08-26 12:04:12 -0400

    made changes to my branch final

--- main.cpp
+++ main.cpp
@@ -25,6 +25,7 @@ int main() {
         cout << "              <(@ )___-" << endl; // student 3(1) may change this line and add additional lines between this line and immediately below
         cout << "               ( .__> /" << endl; // student 3(2) may change this line
         cout << "                `----' " << endl; // TODO student 2 may change this line and may add lines below
+        cout << "                _/ _/  " << endl;
     }
 
     return 0;
